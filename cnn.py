59a2d45bcfa814311d96acbc6769426a8d082434
Rodrigo Torrealba <torrealbacruzrodrigo@gmail.com>
2023-10-19 19:44:19 -0600

    Version 0.003

--- cnn.py
+++ cnn.py
@@ -49,34 +49,77 @@ class CNN:
         return preds
 
      
-    def train(self, X, Y, epochs=1, batch_size=32, learning_rate=0.01):
+    def train(self, X, Y, epochs=10, batch_size=32, learning_rate=0.01):
+        loss_function = CrossEntropyLoss()
+
         for epoch in range(epochs):
             print('Epoch', epoch + 1)
+            total_loss = 0
+            total_correct = 0
+
             for i in range(0, len(X), batch_size):
                 X_batch = X[i:i + batch_size]
                 Y_batch = Y[i:i + batch_size]
-                print('X_batch shape:', X_batch.shape)
-                print('Y_batch shape:', Y_batch.shape)
-                preds = self.forward(X_batch)
-                print('preds shape:', preds.shape)
-                #preds = self.backward(preds, Y_batch, learning_rate)
-                print('preds shape:', preds.shape)
-                #print('Loss:', self.loss(Y_batch, preds))
-                #print('Accuracy:', self.accuracy(Y_batch, preds))
+
+                # Forward pass
+                preds = cnn.forward(X_batch)
+                #loss = loss_function.forward(preds, Y_batch)
+
+                # Backward pass
+                #dLdoutput = loss_function.backward(1)  # Loss gradient is 1 for backpropagation
+                preds = cnn.backward(preds, Y_batch, learning_rate)
+
+                #total_loss += loss
+                #total_correct += np.sum(np.argmax(preds, axis=1) == np.argmax(Y_batch, axis=1))
+
+            # Calculate and print average loss and accuracy for the epoch
+            '''
+            average_loss = total_loss / len(X)
+            accuracy = total_correct / len(X)
+            print('Average Loss:', average_loss)
+            print('Accuracy:', accuracy)
+            '''
 
     def predict(self, X):
         # Realiza predicciones
         preds = self.forward(X)
         return preds
 
-    def accuracy(self, Y, preds):
-        # Calcula la precisión
-        return (np.argmax(preds, axis=1) == np.argmax(Y, axis=1)).mean()
+    def evaluate(self, X, Y):
+        # Evalúa el rendimiento de la CNN
+        preds = self.predict(X)
+        preds = np.argmax(preds, axis=1)
+        Y = np.argmax(Y, axis=1)
+        accuracy = np.mean(preds == Y)
+        return accuracy
     
-    def loss(self, Y, preds):
-        # Calcula la pérdida
-        return -np.mean(Y * np.log(preds))
-    
+class CrossEntropyLoss:
+    def __init__(self):
+        self.loss = None
+        self.softmax_output = None
+        self.target = None
+
+    def forward(self, inputs, target):
+        self.target = target
+        self.softmax_output = self.softmax(inputs)
+        self.loss = self.cross_entropy(self.softmax_output, target)
+        return self.loss
+
+    def backward(self, dLdoutput):
+        dLdinput = self.softmax_output - self.target
+        return dLdinput
+
+    def softmax(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        return probabilities
+
+    def cross_entropy(self, predictions, target):
+        epsilon = 1e-15
+        predictions = np.clip(predictions, epsilon, 1 - epsilon)
+        batch_size = predictions.shape[0]
+        return -np.sum(target * np.log(predictions)) / batch_size
+
 
 class Conv:
     def __init__(self, num_filters, kernel_size, stride, activation):
@@ -98,28 +141,18 @@ class Conv:
         # Inicialización de Glorot (Xavier)
         limit = np.sqrt(6.0 / (input_channels + self.num_filters))
         self.weights = np.random.uniform(-limit, limit, size=(self.num_filters, self.kernel_size, self.kernel_size, input_channels))
-
+        print(self.weights.shape)
         self.biases = np.zeros((1, 1, 1, self.num_filters))
 
     def forward(self, input_data):
         print("Conv forward")
         print(input_data.shape)
         self.input = input_data
-        batch_size, input_height, input_width, input_channels = input_data.shape
         self.initialize(input_data.shape)
-
-        print("input_height: ",input_height)
-        print("input_width: ",input_width)
-        print("input_channels: ",input_channels)
-        print("batch_size: ",batch_size)
-
+        batch_size, input_height, input_width, input_channels = input_data.shape
         output_height = (input_height - self.kernel_size) // self.stride + 1
         output_width = (input_width - self.kernel_size) // self.stride + 1
         self.output = np.zeros((batch_size, output_height, output_width, self.num_filters))
-
-        print("output_height: ",output_height)
-        print("output_width: ",output_width)
-        print("output_shape: ",self.output.shape)
 
         for i in range(output_height):
             for j in range(output_width):
@@ -131,9 +164,73 @@ class Conv:
 
         return self.output
 
-    def backward(self, dLdoutput, Y, learning_rate):
-        pass
 
+    def backward(self, dLdoutput, Y, learning_rate):
+        print("Conv backward")
+        batch_size, output_height, output_width, num_filters = dLdoutput.shape
+        input_height, input_width, input_channels = self.input.shape[1:]
+
+        dLdinput = np.zeros_like(self.input)
+        dLdweights = np.zeros_like(self.weights)
+        dLdbiases = np.sum(dLdoutput, axis=(0, 1, 2), keepdims=True)
+
+        if self.activation == 'relu':
+            dLdoutput = dLdoutput * (self.output > 0)
+
+        for i in range(output_height):
+            for j in range(output_width):
+                input_slice = self.input[:, i * self.stride:i * self.stride + self.kernel_size, j * self.stride:j * self.stride + self.kernel_size, :]
+                for f in range(num_filters):
+                    dLdweights[f, :, :, :] += np.sum(input_slice * (dLdoutput[:, i, j, f])[:, np.newaxis, np.newaxis, np.newaxis], axis=0)
+                    dLdinput[:, i * self.stride:i * self.stride + self.kernel_size, j * self.stride:j * self.stride + self.kernel_size, :] += dLdoutput[:, i, j, f][:, np.newaxis, np.newaxis, np.newaxis] * self.weights[f, :, :, :]
+
+        self.weights -= learning_rate * dLdweights
+        self.biases -= learning_rate * dLdbiases
+
+        return dLdinput
+    
+
+class Pool:
+    def __init__(self, size, stride):
+        self.size = size
+        self.stride = stride
+        self.name = 'Pool'
+        self.input = None
+        self.output = None
+        self.mask = None
+
+    def forward(self, input_data):
+        self.input = input_data
+        batch_size, input_height, input_width, num_channels = input_data.shape
+        output_height = (input_height - self.size) // self.stride + 1
+        output_width = (input_width - self.size) // self.stride + 1
+        self.output = np.zeros((batch_size, output_height, output_width, num_channels))
+        mask = np.zeros_like(input_data)  # Nuevo array mask
+
+        for i in range(output_height):
+            for j in range(output_width):
+                input_slice = input_data[:, i * self.stride:i * self.stride + self.size, j * self.stride:j * self.stride + self.size, :]
+                max_values = np.max(input_slice, axis=(1, 2), keepdims=True)
+                mask[:, i * self.stride:i * self.stride + self.size, j * self.stride:j * self.stride + self.size, :] = (input_slice == max_values[:, np.newaxis, np.newaxis, :])
+                self.output[:, i, j, :] = max_values[:, 0, 0, :]
+
+        self.mask = mask  # Actualiza self.mask con el nuevo array
+
+        return self.output
+
+
+
+    def backward(self, dLdoutput, Y, learning_rate):
+        dLdinput = np.zeros_like(self.input)
+        batch_size, output_height, output_width, num_channels = dLdoutput.shape
+
+        for i in range(output_height):
+            for j in range(output_width):
+                dLdinput_slice = dLdoutput[:, i, j, :]
+                dLdinput_slice = dLdinput_slice[:, np.newaxis, np.newaxis, :]
+                dLdinput[:, i * self.stride:i * self.stride + self.size, j * self.stride:j * self.stride + self.size, :] += dLdinput_slice[:, np.newaxis, np.newaxis, :] * self.mask[:, i * self.stride:i * self.stride + self.size, j * self.stride:j * self.stride + self.size, :]
+
+        return dLdinput
 
 
 
@@ -152,22 +249,15 @@ cnn = CNN(input_shape=(21, 28, 3), num_classes=10, weight_init='random')
 
 # Añade las capas
 cnn.add(Conv(num_filters=32, kernel_size=3, stride=1, activation='relu'))
-'''
-cnn.add(Pool(size=2, stride=2))
+
+cnn.add(Pool(size=2, stride=2))  # Reducirá las dimensiones a la mitad
 cnn.add(Conv(num_filters=32, kernel_size=3, stride=1, activation='relu'))
-cnn.add(Pool(size=2, stride=2))
+cnn.add(Pool(size=2, stride=2))  # Reducirá las dimensiones a la mitad nuevamente
+
+'''
 cnn.add(FC(num_neurons=100, activation='relu'))
 cnn.add(FC(num_neurons=10, activation='softmax'))
 '''
 
-# Muestra un resumen de la CNN
-#cnn.summary()
-
 # Entrena la CNN
 cnn.train(train_X, train_label, epochs=10, batch_size=32, learning_rate=0.01)
-
-
-
-
-
-
